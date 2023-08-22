c83ae4bfb10b38e5bc813ea3fc01b2e19f7cef98
Abhishek Rana <96675677+AbhishekRana21@users.noreply.github.com>
2023-08-22 17:29:01 +0530

    Updated p3z.c

--- p3z.c
+++ p3z.c
@@ -6,11 +6,13 @@ int main(){
 	if(x == 0){
 		sleep(5);
 		printf("Child Process ID: %d\n", getpid());
+		printf("Child Terminated After 5 Seconds");
 	}
 	else{
 		
 		printf("Parent Process ID: %d\n", getpid());
 		printf("Child has become a zombie");
+		printf("Parent Terminated");
 	}
 	return 0;
 }
