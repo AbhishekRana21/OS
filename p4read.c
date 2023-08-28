4f4dad251c3a7dcdc68d7a166e976a00bc8b754c
Abhishek Rana <96675677+AbhishekRana21@users.noreply.github.com>
2023-08-28 17:19:47 +0530

    Created p4read.c

---
+++ p4read.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	int fd, sz;
+	char* c = (char*)calloc(100, sizeof(char));
+
+	fd = open("foo.txt", O_RDONLY);
+	if (fd < 0) {
+		perror("r1");
+		exit(1);
+	}
+
+	sz = read(fd, c, 11);
+	printf("called read(% d, c, 11). returned that"
+		" %d bytes were read.\n",
+		fd, sz);
+	c[sz] = '\0';
+	printf("Those bytes are as follows: % s\n", c);
+
+	return 0;
+}
