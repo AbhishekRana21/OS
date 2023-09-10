0ba198206e6cfe3d95acb043969abd99c7adea4b
Abhishek Rana <96675677+AbhishekRana21@users.noreply.github.com>
2023-09-10 18:13:51 +0530

    Updated p3o.c

--- p3o.c
+++ p3o.c
@@ -4,15 +4,15 @@
 int main(){
 	int x = fork();
 	if(x == 0){
+		sleep(5);
 		printf("Child Process ID: %d\n", getpid());
 		printf("Child is Orphan\n");
-		printf("Child Terminated");
+		printf("Child Terminated After 5 Seconds");
 		
 	}
 	else{
-		sleep(5);
 		printf("Parent Process ID: %d\n", getpid());
-		printf("Parent Terminated After 5 Seconds");
+		printf("Parent Terminated");
 	}
 	return 0;
 }
